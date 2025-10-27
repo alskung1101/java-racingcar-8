6fdfe840e82c70d702905f7da55b2106cb772e07
alskung1101 <alskung1101@naver.com>
2025-10-27 19:54:30 +0900

    feat: Controller 및 Application 구현으로 게임 흐름 통합

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,52 @@
+package racingcar.controller;
+
+import racingcar.model.Cars;
+import racingcar.model.MissionUtilsRandomGenerator;
+import racingcar.model.RandomGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class GameController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private Cars cars;
+    private int tryCount;
+
+    public void run() {
+        try {
+            initializeGame();
+            startGame();
+            endGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println("ERROR " + e.getMessage());
+
+        }
+    }
+
+    private void initializeGame() {
+
+        this.cars = createCars();
+        this.tryCount = inputView.inputTryCount();
+    }
+
+    private Cars createCars() {
+        RandomGenerator generator = new MissionUtilsRandomGenerator();
+        return new Cars(inputView.inputCarNames(), generator);
+    }
+
+    private void startGame() {
+        outputView.printExecutionStart();
+        for (int i = 0; i < tryCount; i++) {
+            runOneRound();
+        }
+    }
+
+    private void runOneRound() {
+        cars.executeRound();
+        outputView.printRoundResult(cars.getCars());
+    }
+
+    private void endGame() {
+        outputView.printWinners(cars.getWinners());
+    }
+}
